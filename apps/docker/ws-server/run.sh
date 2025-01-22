5107b69c409d9ab24036780a50afed45f2b083f7
TonStockFi <barry.ptp@gmail.com>
2025-01-22 23:43:55 +0630

    add webrtc

--- apps/docker/ws-server/run.sh
+++ apps/docker/ws-server/run.sh
@@ -1,8 +1,8 @@
-cd /root/projects/web3-desk/apps/desktop/docker/ws-server
+cd /root/web3-desk/apps/docker/ws-server
 git pull origin main 
-cp ../../src/electron/ws-server/server.ts ./
-cp ../../src/electron/ws-server/BufferProcessor.ts ./
-# cp ../../src/electron/ws-server/main.ts ./
+cp ../desktop/src/electron/ws-server/server.ts ./
+cp ../desktop//src/electron/ws-server/BufferProcessor.ts ./
+# cp ../desktop//src/electron/ws-server/main.ts ./
 
 
 docker rm -f ctl-server
